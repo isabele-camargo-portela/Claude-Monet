160adb6a694452ddc2539436b09c85c5fd5983b8
Isa <isabelecamargoportela@gmail.com>
2026-09-04 19:56:56 -0300

    feat: conclui primeira versão do site sobre Claude Monet

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
+//carrocel home
+
 const cards = document.getElementById("cards");
 const left = document.getElementById("left");
 const right = document.getElementById("right");
 
 right.addEventListener("click", function() {
     cards.scrollBy({
-        left: 360,
+        left: 320,
         behavior: "smooth"
     });
 });
 
 left.addEventListener("click", function() {
     cards.scrollBy({
-        left: -360,
+        left: -320,
         behavior: "smooth"
     });
 });
