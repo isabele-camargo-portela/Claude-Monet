2973d2f0f45d3333c197b6371c9c7c0b25b6834f
Isa <isabelecamargoportela@gmail.com>
2026-09-04 13:46:09 -0300

    feat: adiciona primeira versão do site sobre Claude Monet

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+const cards = document.getElementById("cards");
+const left = document.getElementById("left");
+const right = document.getElementById("right");
 
+right.addEventListener("click", function() {
+    cards.scrollBy({
+        left: 360,
+        behavior: "smooth"
+    });
+});
+
+left.addEventListener("click", function() {
+    cards.scrollBy({
+        left: -360,
+        behavior: "smooth"
+    });
+});
